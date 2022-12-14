2c8de7c3be1eeb56918342ebcd4874cda7ea0947
Lucino772 <lucapalmi772@gmail.com>
2022-12-14 18:06:06 +0100

    refactor(python-example): removed old example code

--- liblora-python/example.py
+++ liblora-python/example.py
@@ -11,35 +11,6 @@ def on_receive(pkt):
         print("Something went wrong !")
 
 def main():
-    # driver = _liblora.liblora_driver_wiringpi(0, 6)
-    # if driver is None:
-    #     print('Failed to create wiringPi driver')
-    #     return 1
-    
-    # radio = _liblora.liblora_rf95_radio(driver, 7, 6)
-    # if radio is None:
-    #     print('Failed to create radio')
-    #     return 1
-
-    # freq = 868100000 # 8681E5
-    # if _liblora.liblora_rf95_init(radio, freq, 0x07, 0x07) != 0:
-    #     print('Failed to initialize radio')
-    #     return 1
-
-    # _liblora.liblora_rf95_onrx(radio, _liblora.LIBLORA_RF95_RX_CALLBACK(on_receive))
-
-    # if not _liblora.liblora_rf95_recv(radio, True):
-    #     print("Failed to put in RX_CONT mode")
-
-    # try:
-    #     while True:
-    #         pass
-    # except KeyboardInterrupt:
-    #     pass
-
-    # _liblora.liblora_rf95_sleep(radio)
-    # _liblora.liblora_rf95_delete(radio)
-
     try:
         radio.init(868100000, 0x07, 0x07)
     except RuntimeError:
