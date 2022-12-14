a7f6388358a3fd694cb14829392461a92cc835a4
Lucino772 <lucapalmi772@gmail.com>
2022-12-14 11:52:23 +0100

    fix(python): remove unused os.environ tweak

--- liblora-python/lora/_liblora.py
+++ liblora-python/lora/_liblora.py
@@ -1,9 +1,5 @@
-import os
-
 import ctypes
 import ctypes.util
-
-os.environ['LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH']
 
 liblora_so = ctypes.util.find_library('lora')
 liblora = ctypes.CDLL(liblora_so)
