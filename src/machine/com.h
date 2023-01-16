e19c673ef4c7c746235c1ec362f6219d57b35ecb
Lucino772 <lucapalmi772@gmail.com>
2023-01-16 16:33:57 +0100

    chore: added implementation for com.h

--- src/machine/com.h
+++ src/machine/com.h
@@ -2,22 +2,31 @@
 #define _LIBLORA_MACHINE_COM_H_
 
 #include <stdint.h>
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
 
-    int liblora_com_open(void* dev);
-    int liblora_com_transfer(void* dev, uint8_t* buffer, int len);
+    typedef struct
+    {
+        int spi_dev;
+        int spi_ss;
+        int spi_clck_speed;
+        bool usb;
+    } liblora_com_dev_t;
+
+    int liblora_com_open(liblora_com_dev_t* dev);
+    int liblora_com_transfer(liblora_com_dev_t* dev, uint8_t* buffer, int len);
 
     // SPI
-    int liblora_spi_open(void* dev);
-    int liblora_spi_transfer(void* dev, uint8_t* buffer, int len);
+    int liblora_spi_open(liblora_com_dev_t* dev);
+    int liblora_spi_transfer(liblora_com_dev_t* dev, uint8_t* buffer, int len);
 
     // USB
-    // int liblora_usb_open(void* dev);
-    // int liblora_usb_transfer(void* dev, uint8_t* buffer, int len);
+    // int liblora_usb_open(liblora_com_dev_t* dev);
+    // int liblora_usb_transfer(liblora_com_dev_t* dev, uint8_t* buffer, int len);
 
 #ifdef __cplusplus
 }
