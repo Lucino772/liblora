c9ea404da61eee628e09140889d3ec9968429da6
Lucino772 <lucapalmi772@gmail.com>
2023-01-16 10:21:48 +0100

    feat: added liblora_com_open function

--- src/machine/com.h
+++ src/machine/com.h
@@ -8,8 +8,15 @@ extern "C"
 {
 #endif
 
+    int liblora_com_open(void* dev);
     int liblora_com_transfer(void* dev, uint8_t* buffer, int len);
+
+    // SPI
+    int liblora_spi_open(void* dev);
     int liblora_spi_transfer(void* dev, uint8_t* buffer, int len);
+
+    // USB
+    // int liblora_usb_open(void* dev);
     // int liblora_usb_transfer(void* dev, uint8_t* buffer, int len);
 
 #ifdef __cplusplus
